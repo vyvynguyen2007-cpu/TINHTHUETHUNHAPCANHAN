a678123ee3e99304ec691e582fbb579f21e240dd
vyvynguyen2007-cpu <vyvynguyen2007@gmail.com>
2026-07-17 13:36:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def tinh_thue_tncn(gross, bonus, overtime, lunch, other, deps):
     tax_breakdown = []
     for b in brackets:
       range_size = b["limit"] - previous_limit
-        if temp_income > 0:
+    if temp_income > 0:
             taxable_in_bracket = min(temp_income, range_size)
             tax_in_bracket = taxable_in_bracket * b["rate"]
             tax += tax_in_bracket
